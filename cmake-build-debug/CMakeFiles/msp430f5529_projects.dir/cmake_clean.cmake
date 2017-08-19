a12aca49a6272f4b6a63a334dc9b22f3e48dc009
Adam@NewX <adam.allaf@hotmail.com>
2017-08-19 18:39:38 +0200

    Added CLion configs

---
+++ cmake-build-debug/CMakeFiles/msp430f5529_projects.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/msp430f5529_projects"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/msp430f5529_projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
